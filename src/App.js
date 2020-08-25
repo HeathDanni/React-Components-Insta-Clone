e889175ffc5d6374256e73f43ca76916988d0c15
Heather Beckman <heatherbeckman85@gmail.com>
2020-08-25 09:28:04 -0600

    got number of likes to display properly

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ const App = () => {
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = postId => {
+
+    
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
